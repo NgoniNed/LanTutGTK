739699a39730931d141e82df98fb7a99c6ba473e
NNedziwe <ngonidzashenedziwe@gmail.com>
2020-08-25 05:38:10 +0200

    version 1 revised

--- LanTutor/LTPhaseOneCore.cs
+++ LanTutor/LTPhaseOneCore.cs
@@ -41,6 +41,109 @@ namespace LanTutor
         public LTPhaseOneCore()
         {
         }*/
+
+        internal static void ExecuteProgramBackend()
+        {
+            if (!(Directory.Exists(Environment.CurrentDirectory + "/ReportCards")) || (Directory.GetFiles(Environment.CurrentDirectory + "/ReportCards").Length <= 0))
+            {
+                LTPhaseOneCore.LanTutEnvironmentSetup();
+            }
+            else
+            {
+                Console.WriteLine("Environment Is Ready...\n Preparing session");
+            }
+            //
+            //get the reportcard of current user
+            //load question for user
+            /*
+             * lantutxmlmoving
+             *      loadxmlfile
+             *      getall the session questions
+             *      getcurrentquestion
+             */
+            System.Xml.XmlNodeList nodeList = LanTutorXMLMoving.LoadSessionQuestions(LanTutorXMLMoving.LoadXMLFile(Directory.GetFiles(Environment.CurrentDirectory + "/ReportCards")[0]), "WordTransDefLibrary/SessionLibrary/WordTransDef");
+            Console.WriteLine(nodeList.Count);
+            WordTransDef currentQuestion = LanTutorXMLMoving.GetCurrentQuestionl(5, ref nodeList);
+            bool userQ = currentQuestion.PrintInfo;
+            /*/update score parameters
+            currentQuestion.lWordScore = new ScoreParameters()
+            {
+                Attempts = 5,
+                Score = 2,
+                TimeSpent = "6"
+            };
+            currentQuestion.lDescriptionScore = new ScoreParameters()
+            {
+                Attempts = 3,
+                Score = 2,
+                TimeSpent = "10"
+            };
+            userQ = currentQuestion.PrintInfo;*/
+            //get previous question
+            currentQuestion = LanTutorXMLMoving.GetPreviousQuestionl(5, ref nodeList);
+            userQ = currentQuestion.PrintInfo;
+            //get next question
+            currentQuestion = LanTutorXMLMoving.GetNextQuestionl(5, ref nodeList);
+            userQ = currentQuestion.PrintInfo;
+            //done learning
+            currentQuestion = LanTutorXMLMoving.GetCurrentQuestionl(5, ref nodeList);
+            //update score parameters
+            currentQuestion.lWordScore = new ScoreParameters()
+            {
+                Attempts = 5,
+                Score = 2,
+                TimeSpent = "6"
+            };
+            currentQuestion.lDescriptionScore = new ScoreParameters()
+            {
+                Attempts = 3,
+                Score = 2,
+                TimeSpent = "10"
+            };
+            userQ = currentQuestion.PrintInfo;
+            LanTutorXMLMoving.UpdateCurrentNodeList(currentQuestion, 5, ref nodeList);
+            Console.WriteLine(nodeList.Count);
+            currentQuestion = LanTutorXMLMoving.GetCurrentQuestionl(5, ref nodeList);
+            userQ = currentQuestion.PrintInfo;
+            /*
+             * flush usersessionreportcard to file
+             *      over write the whole reportcard
+             */
+            Console.WriteLine(Directory.GetFiles(Environment.CurrentDirectory + "/ReportCards")[0]);
+            LTWriteFile.WriteNodeListToXml(Directory.GetFiles(Environment.CurrentDirectory + "/ReportCards")[0], nodeList);
+            //LTPhaseOneCore.DataPrep();
+            /*
+             * load LanTut dictionary in memory
+             * prepare LTScoreCard for the current session
+             * save session scorecardreport to file
+             */
+            //string[] lfiles = Directory.GetFiles("/Volumes/Secondary/Projects/PersonalGTK/LanTutor/LanTutor/Dictionary/");
+            /*string lusername = "Ngoni1";
+            
+            string dict = "eng-rus_LanTut_.xml";
+            Console.Clear();
+            Console.WriteLine("New Session");
+            //PrepareUserReportCard(lmainDirectory,lusername);
+            /*XmlNodeList mySessionLists = LanTutorXMLMoving.LoadSessionQuestions(LanTutorXMLMoving.LoadXMLFile(lmainDirectory+"/ReportCards/Ngoni_ReportCard.xml"));
+            
+            Console.WriteLine(mySessionLists.Count);
+            int ii = 1;
+            WordTransDefDict qtouser = LanTutorXMLMoving.GetCurrentQuestion(ref ii,ref mySessionLists);
+            bool tmp = qtouser.PrintInfo;
+
+            WordTransDefDict nxtqtouser = LanTutorXMLMoving.GetNextQuestion(ref ii, ref mySessionLists);
+            tmp = nxtqtouser.PrintInfo;
+
+            WordTransDefDict prvqtouser = LanTutorXMLMoving.GetPreviousQuestion(ref ii, ref mySessionLists);
+            tmp = prvqtouser.PrintInfo;
+            
+            LTScoreCard lT = new LTScoreCard();
+            lT.SessionLibrary = myset;
+            LTWriteFile.WriteSchemeToxml(lT, "/Volumes/Secondary/Projects/PersonalGTK/LanTutor/LanTutor/Dictionary");
+            WriteXmlData();
+            */
+        }
+
         internal static void LanTutEnvironmentSetup()
         {
 
